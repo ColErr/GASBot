d7497c3f8c44ca8519e26d9301ad11b80c30c7f2
ColErr <ColErr@users.noreply.github.com>
2020-06-13 21:01:48 -0700

    Added functionality to create database and table, and being matched adds you to the database.

--- gasbot.py
+++ gasbot.py
@@ -7,12 +7,12 @@ from deck import Deck
 
 class GASBot:
     
-    def __init__(self, bot_token, database):
+    def __init__(self, bot_token, databasename):
         self.token = bot_token
-        self.database = database
         self.lastResetTime = datetime.now()
+        self.setup_db(databasename)
         
-        self.lfgQueue = []
+        self.lfgqueue = []
         
         self.bot = commands.Bot(command_prefix="!", case_insensitive=True)
         
@@ -22,6 +22,8 @@ class GASBot:
         self.setup_listeners()
         
         self.bot.run(self.token)
+        
+        self.database.close()
         
     def setup_listeners(self):
         @self.bot.event
@@ -48,15 +50,64 @@ class GASBot:
             async with ctx.channel.typing():
                 deckID = Deck.importDeck(decknum)
             
-            if len(self.lfgQueue) == 0:
-                self.lfgQueue.append([ctx.author.id, deckID])
+            if len(self.lfgqueue) == 0:
+                self.lfgqueue.append([ctx.author.id, deckID])
                 await ctx.send("You have joined the queue")
             else:
-                match = self.lfgQueue.pop(0)
+                match = self.lfgqueue.pop(0)
                 opp = await self.bot.fetch_user(match[0])
+                self.database.execute('''
+                    INSERT INTO matches(player1, p1deck, p1wins, p1confirm, player2, p2deck, p2wins, p2confirm)
+                    VALUES(?, ?, 0, 0, ?, ?, 0, 0);
+                    ''', (match[0], match[1], ctx.author.id, deckID))
+                self.database.commit()
                 message = ctx.author.mention + ", you are playing " + opp.mention
                 await ctx.send(message)
+            return
             
+        @self.bot.command(name="leavequeue")
+        async def leavequeue(ctx):
+            i=0
+            for x in self.lfgqueue:
+                if ctx.author.id == x[0]:
+                    self.lfgqueue.pop(i)
+                    await ctx.send(ctx.author.mention + ", you have been dropped from the queue")
+                    return
+                else:
+                    i += 1
+            await ctx.send(ctx.author.mention + ", you are not in the queue")
+            
+    def setup_db(self, name):
+        self.database = None
+        # Establish DB connection
+        try:
+            self.database = sqlite3.connect(name)
+        except Error as e:
+            print(e)
+            quit()
+        
+        # Check to see if tables are set up
+        c = self.database.cursor()
+        c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='matches' ")
+        if c.fetchone()[0] != 1:
+            self.database.execute('''
+                CREATE TABLE matches(
+                id INTEGER PRIMARY KEY,
+                player1 INT NOT NULL,
+                p1deck TEXT NOT NULL,
+                p1wins INT NOT NULL,
+                p1confirm NUMERIC,
+                player2 INT NOT NULL,
+                p2deck TEXT NOT NULL,
+                p2wins INT NOT NULL,
+                p2confirm NUMERIC);
+            ''')
+            print("Created matches table")
+        else:
+            c.execute
+        
+        return
+    
     def setup_cogs(self):
         return
         
